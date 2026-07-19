1f00d0314cb0c4ded9c19b08ddd9728ba22af3cd
Mohit Sahani <mohitnishad81@gmail.com>
2026-07-19 10:23:25 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/scm/config/CorsConfig.java
+++ backend/src/main/java/com/scm/config/CorsConfig.java
@@ -23,7 +23,23 @@ public class CorsConfig {
                         .allowedOrigins(
                                 "http://localhost:5500",
                                 "http://127.0.0.1:5500",
+                                " @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500",
                                 "https://smartcontactmanager-1-0gcm.onrender.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
